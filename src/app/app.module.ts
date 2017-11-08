46242517040194a0c8ec757869a4137e0be40ceb
Arthur <AmeliePoulain-92@mail.ru>
2017-11-08 22:49:12 +0200

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { IntroComponent } from './intro/intro.component';
 import { JazzmenComponent } from './jazzmen/jazzmen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntroComponent,
     JazzmenComponent
   ],
   imports: [
